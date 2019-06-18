615916cc4b1e848e077c99856015873c9f2cd26b
jlacko <jindra.lacko@gmail.com>
2019-06-18 13:38:29 +0000

    retweety vráceny zpět, mají poloviční váhu proti originálu

--- generate-chart.R
+++ generate-chart.R
@@ -1,18 +1,24 @@
 # z objektu tweets vytvoří a uloží obrázek
 
 # Vlastní těžení...
-slova <- udpipe_annotate(udmodel, x = filter(tweets, !is_retweet)$text) %>% # UDPIPE provede svojí magii...
+slova <- udpipe_annotate(udmodel, x = tweets$text, doc_id = tweets$status_id) %>% # UDPIPE provede svojí magii...
   as.data.frame() %>%
   subset(upos %in% c('NOUN', 'VERB', 'PROPN', 'ADJ', 'ADV') & !lemma %in% balast) # balast = stopwords definované v main.R
 
+retweety <- tweets %>% 
+  mutate(vaha = ifelse(is_retweet, 1/2, 1)) %>%  # váha retweetu je poloviční proti originálu
+  select(status_id, vaha)
+
 freq <- slova %>%
-  count(lemma) %>%
+  inner_join(retweety, by = c("doc_id" = "status_id")) %>% # připojit váhy podle idčka tweetu
+  group_by(lemma) %>%
+  summarize(n = sum(vaha)) %>% 
   arrange(desc(n))
 
 if (freq[1,2] > 1.5*freq[2,2]) freq[1,2] <- 1.5*freq[2,2]  # snížit prominenci prvního slova, if necessary
 
 library(wordcloud)
 png(filename = "~/babisobot/wcloud.png", width = 800, height = 800/1.91, res = 100)
-  wordcloud(freq$lemma, freq$n, max.words = 100, scale = c(3.25, 0.5),
+  wordcloud::wordcloud(freq$lemma, freq$n, max.words = 100, scale = c(3, 0.5),
             random.order = F, colors=rev(brewer.pal(6,"RdYlGn")))
 dev.off()
