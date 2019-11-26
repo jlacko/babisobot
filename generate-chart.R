e4b2ac40feb52473479ebb62749320c645ea2bfd
jlacko <jindra.lacko@gmail.com>
2019-11-26 07:25:23 +0000

    zobecnění výjimek z lemmatizace

--- generate-chart.R
+++ generate-chart.R
@@ -3,7 +3,7 @@
 # Vlastní těžení...
 slova <- udpipe_annotate(udmodel, x = tweets$text, doc_id = tweets$status_id, parallel.cores = 2) %>% # UDPIPE provede svojí magii...
   as.data.frame() %>%
-  mutate(lemma = ifelse(token == "Krym", "Krym", lemma)) %>% # Krym je Krym, ne Kry... (... a syna dáváš katu) 
+  mutate(lemma = ifelse(token %in% excepto_lemmas, token, lemma)) %>% # pro tyto tokeny se nepoužije lemma, ale sám token
   subset(upos %in% c('NOUN', 'VERB', 'PROPN', 'ADJ', 'ADV') & !lemma %in% balast) # balast = stopwords definované v main.R
 
 retweety <- tweets %>% 
